01b65659bcfff8e91151e3087d83b965f81d2e8e
Christophe Laudon <ch.laudon@gmail.com>
2023-02-27 08:58:44 +0100

    Changement de syntaxe pour l'évaluation Custom des modifiers

--- module/system/utils.mjs
+++ module/system/utils.mjs
@@ -1,4 +1,4 @@
-import { CO } from "./config.mjs";
+import { Log } from "../utils/log.mjs";
 
 export class Utils {
   static shortcutResolve(shortcut) {
@@ -15,8 +15,8 @@ export class Utils {
 
   /**
    * @name getTooltip
-   * @param {*} name 
-   * @param {*} value 
+   * @param {*} name
+   * @param {*} value
    * @returns {String} Chaine vide ou "Nom : valeur "
    */
   static getTooltip(name, value) {
@@ -32,12 +32,12 @@ export class Utils {
 
   /**
    * @description For an actor, evaluate the formula
-   * @param {*} actor 
-   * @param {*} formula 
+   * @param {*} actor
+   * @param {*} formula
    * @param {*} source The item source's UUID : used for the #rank
-   * @returns 
+   * @returns
    */
-  static evaluate(actor, formula, source){
+  static evaluate(actor, formula, source) {
     if (formula === "") return 0;
     if (formula.includes("@") || formula.includes("#")) return this._evaluateCustom(actor, formula, source);
     const resultat = parseInt(formula);
@@ -45,50 +45,60 @@ export class Utils {
     return resultat;
   }
 
-   /**
+  /**
    * @description Evaluate a custom value
+   * Shortcuts
+   * @str @dex @con @int @wis @cha @mel @ran @mag @lvl @rank[+1,0,+1,0,0]
    * @param {*} actor
    * @returns {int} the modifier's value
    */
-     static _evaluateCustom(actor, formula, source) {
-      if (!formula.includes("@") && !formula.includes("#")) return value;
-      let replacedFormula = formula;
+  static _evaluateCustom(actor, formula, source) {
+    if (!formula.includes("@")) return value;
 
-      // #level
-      if (formula.includes("#level")) {
-        replacedFormula = replacedFormula.replace("#level", actor.system.attributes.level.value);
-      }
-  
-      // #rank{+1,0,+1,0,0}
-      if (formula.includes("#rank")) {
-        let startRank = formula.substring(formula.indexOf("#rank"));
-        let extract = startRank.substring(formula.indexOf("[") + 1, formula.indexOf("]"));
-        let ranks = extract.split(",");
-        let itemSource = fromUuidSync(source);
-        let rank = itemSource.system.rank;
-        let total = 0;
+    Log.debug("Custom Formula : ", formula);
+
+    let replacedFormula = formula;
+
+    // Shortcuts
+    if (replacedFormula.includes("@for")) replacedFormula = replacedFormula.replace("@for", actor.system.abilities.str.mod);
+    if (replacedFormula.includes("@dex")) replacedFormula = replacedFormula.replace("@dex", actor.system.abilities.dex.mod);
+    if (replacedFormula.includes("@con")) replacedFormula = replacedFormula.replace("@con", actor.system.abilities.con.mod);
+    if (replacedFormula.includes("@int")) replacedFormula = replacedFormula.replace("@int", actor.system.abilities.int.mod);
+    if (replacedFormula.includes("@sag")) replacedFormula = replacedFormula.replace("@sag", actor.system.abilities.wis.mod);
+    if (replacedFormula.includes("@cha")) replacedFormula = replacedFormula.replace("@cha", actor.system.abilities.cha.mod);
+
+    if (replacedFormula.includes("@atc")) replacedFormula = replacedFormula.replace("@atc", actor.system.combat.melee.mod);
+    if (replacedFormula.includes("@atd")) replacedFormula = replacedFormula.replace("@atd", actor.system.combat.ranged.mod);
+    if (replacedFormula.includes("@atm")) replacedFormula = replacedFormula.replace("@atm", actor.system.combat.magic.mod);
+    if (replacedFormula.includes("@def")) replacedFormula = replacedFormula.replace("@def", actor.system.combat.def.value);
+
+    if (replacedFormula.includes("@niv")) replacedFormula = replacedFormula.replace("@niv", actor.system.attributes.level.value);
+
+    if (replacedFormula.includes("@rang")) {
+      let startRank = replacedFormula.substring(replacedFormula.indexOf("@rang"));
+      let extract = startRank.substring(replacedFormula.indexOf("[") + 1, replacedFormula.indexOf("]"));
+      let ranks = extract.split(",");
+      let itemSource = actor.items.get(source);
+      const pathId = itemSource.system.path;
+      const path = actor.items.get(pathId);
+      const rank = path.system.rank;
+      let total = 0;
+      if (rank) {
         for (let index = 0; index < rank; index++) {
           const element = ranks[index];
           let val = parseInt(element);
           if (val) total += val;
         }
-        replacedFormula = replacedFormula.replace("#rank[" + extract + "]", total) ;
       }
-  
-      // #mod{str}
-      if (formula.includes("#mod")) {
-        let ability = formula.substring(formula.indexOf("[") + 1, formula.indexOf("]"));
-        replacedFormula = replacedFormula.replace("#mod[" + ability + "]", "actor.system.abilities." + ability + ".mod" );
-      }
-  
-      // @abilities.str.value
-      if (formula.includes("@")) {
-        replacedFormula = replacedFormula.replace("@", "actor.system.");
-      }
-
-      return eval(replacedFormula);
+      replacedFormula = replacedFormula.replace("@rang[" + extract + "]", total);
     }
 
+    // Remaining formula
+    if (replacedFormula.includes("@")) {
+      replacedFormula = replacedFormula.replace("@", "actor.system.");
+    }
 
-
+    Log.debug("Custom Formula evaluated : ", replacedFormula);
+    return eval(replacedFormula);
+  }
 }
