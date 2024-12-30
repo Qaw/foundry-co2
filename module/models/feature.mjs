589c0e21d4be53c408b997e366821ebbc8865c4f
Christophe Laudon <ch.laudon@gmail.com>
2024-12-30 09:49:13 +0100

    Ajout de localisation

--- module/models/feature.mjs
+++ module/models/feature.mjs
@@ -4,14 +4,10 @@ export default class FeatureData extends ItemData {
   static defineSchema() {
     const fields = foundry.data.fields
     return foundry.utils.mergeObject(super.defineSchema(), {
-      subtype: new fields.StringField({
-        required: true,
-        nullable: false,
-        initial: "",
-      }),
+      subtype: new fields.StringField({ required: true }),
       modifiers: new fields.ArrayField(new fields.ObjectField()),
-      paths: new fields.ArrayField(new fields.StringField()),
-      capacities: new fields.ArrayField(new fields.StringField()),
+      paths: new fields.ArrayField(new fields.DocumentUUIDField({ type: "Item" })),
+      capacities: new fields.ArrayField(new fields.DocumentUUIDField({ type: "Item" })),
     })
   }
 }
