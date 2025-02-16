a29fcf2e96cd820d1d732bb62c5a93c5f0ba7701
Christophe Laudon <ch.laudon@gmail.com>
2025-02-16 10:15:41 +0100

    Prise en charge du dé évolutif et du rang dans une voie

--- module/utils.mjs
+++ module/utils.mjs
@@ -52,6 +52,11 @@ export default class Utils {
    */
   static evaluateWithDice(actor, formula, source) {
     if (formula === "") return ""
+
+    if (formula.includes("d4°")) {
+      formula = Utils._replaceEvolvingDice(actor, formula)
+    }
+
     if (formula.includes("@")) return this._evaluateCustom(actor, formula, source, true, true)
     return formula
   }
@@ -140,41 +145,12 @@ export default class Utils {
       })
     }
 
-    /**
-     * Calculates the total rank based on a given formula and keyword.
-     *
-     * This function searches for a keyword within the provided formula and extracts
-     * a list of ranks enclosed in square brackets. It then sums up the ranks up to
-     * the current rank of the path associated with the actor's item source.
-     *
-     * @param {string} replacedFormula The formula string that may contain the keyword and ranks.
-     * @param {string} keyword The keyword to search for within the formula.
-     * @returns {string} - The updated formula with the total rank calculated and replaced.
-     */
-    function calculateTotalRank(replacedFormula, keyword) {
-      if (replacedFormula.includes(keyword)) {
-        let startRank = replacedFormula.substring(replacedFormula.indexOf(keyword))
-        let extract = startRank.substring(startRank.indexOf("[") + 1, startRank.indexOf("]"))
-        let ranks = extract.split(",")
-        let itemSource = actor.items.get(source)
-        const pathId = itemSource.system.path
-        const path = actor.items.get(pathId)
-        const rank = path.system.rank
-        let total = 0
-        if (rank) {
-          for (let index = 0; index < rank; index++) {
-            const element = ranks[index]
-            let val = parseInt(element)
-            if (val) total += val
-          }
-        }
-        replacedFormula = replacedFormula.replace(`${keyword}[${extract}]`, total)
-      }
-      return replacedFormula
-    }
+    if (replacedFormula.includes("@rang") || replacedFormula.includes("@rank")) replacedFormula = _replaceRank(actor, replacedFormula, source)
+    if (replacedFormula.includes("@allrang") || replacedFormula.includes("@allrank")) replacedFormula = _replaceAllRank(actor, replacedFormula, source)
 
-    replacedFormula = calculateTotalRank(replacedFormula, "@rang")
-    replacedFormula = calculateTotalRank(replacedFormula, "@rank")
+    /* ancien system avec @rang[0,1,0,0,1]
+    replacedFormula = this.calculateTotalRank(replacedFormula, "@rang")
+    replacedFormula = this.calculateTotalRank(replacedFormula, "@rank")*/
     console.log(replacedFormula)
     // Remaining formula
     if (replacedFormula.includes("@")) {
@@ -186,5 +162,126 @@ export default class Utils {
       if (toEvaluate) return eval(replacedFormula)
       else return replacedFormula
     }
+  }
+
+  /**
+   * Calculates the total rank based on a given formula and keyword.
+   *
+   * This function searches for a keyword within the provided formula and extracts
+   * a list of ranks enclosed in square brackets. It then sums up the ranks up to
+   * the current rank of the path associated with the actor's item source.
+   *
+   * @param {string} replacedFormula The formula string that may contain the keyword and ranks.
+   * @param {string} keyword The keyword to search for within the formula.
+   * @returns {string} - The updated formula with the total rank calculated and replaced.
+   */
+  static calculateTotalRank(replacedFormula, keyword) {
+    if (replacedFormula.includes(keyword)) {
+      let startRank = replacedFormula.substring(replacedFormula.indexOf(keyword))
+      let extract = startRank.substring(startRank.indexOf("[") + 1, startRank.indexOf("]"))
+      let ranks = extract.split(",")
+      let itemSource = actor.items.get(source)
+      const pathId = itemSource.system.path
+      const path = actor.items.get(pathId)
+      const rank = path.system.rank
+      let total = 0
+      if (rank) {
+        for (let index = 0; index < rank; index++) {
+          const element = ranks[index]
+          let val = parseInt(element)
+          if (val) total += val
+        }
+      }
+      replacedFormula = replacedFormula.replace(`${keyword}[${extract}]`, total)
+    }
+    return replacedFormula
+  }
+
+  /**
+   * Retourne un texte dans lequel on a remplacé le dé évolutif par le dé qu'il devrait y avoir selon le niveau de l'acteur
+   * @param {*} actor : acteur concerné
+   * @param {*} content : le texte contenant un dé évolutif
+   * @returns un temps avec le vrai dé à utiliser
+   */
+  static _replaceEvolvingDice(actor, content) {
+    let result = content
+    if (actor.attributes.level < 6) result = content.replace("d4°", "d4")
+    else if (actor.attributes.level <= 8) result = content.replace("d4°", "d6")
+    else if (actor.attributes.level <= 11) result = content.replace("d4°", "d8")
+    else if (actor.attributes.level <= 14) result = content.replace("d4°", "d10")
+    else result = content.replace("d4°", "d12")
+    return result
+  }
+
+  /**
+   * Retourne un texte dans lequel on a remplacé le rang dans une categorie par sa valeur
+   * @param {*} actor : acteur concerné
+   * @param {*} content : le texte contenant une référence à un rang
+   * @param {UUID} source The item source's UUID : used for the #rank
+   */
+  static _replaceRank(actor, content, source) {
+    //Pour connaitre le rang d'une voie il faut remonter à la source pour savoir de quelle voie il s'agit
+    let itemSource = actor.items.get(source)
+    if (CONFIG.debug.co?.rolls) console.debug(Utils.log(`Utils - _replaceRank - itemSource`), itemSource)
+    const pathId = itemSource.system.path
+    const path = actor.items.get(pathId) //la voie du joueur
+    const rank = path.system.rank //le rang qu'il a dans la voie
+    if (content.includes("@rang")) content = content.replace("@rang", rank)
+    if (content.includes("@rank")) content = content.replace("@rank", rank)
+    return content
+  }
+
+  /**
+   * Retourne un texte dans lequel on a remplacé @allrank[level] par le nombre de fois qu'un rang dans le meme profil atteinte le level indiqué
+   * @param {*} actor : acteur concerné
+   * @param {*} content : le texte contenant une référence à un rang
+   * @param {UUID} source The item source's UUID : used for the #rank
+   */
+  static _replaceAllRank(actor, content, source) {
+    let startRank = replacedFormula.substring(replacedFormula.indexOf("@allrank"))
+    let extract = startRank.substring(startRank.indexOf("[") + 1, startRank.indexOf("]"))
+    if (extract) {
+      let level = parseInt(extract)
+      if (level) {
+        let itemSource = actor.items.get(source)
+        if (CONFIG.debug.co?.rolls) console.debug(Utils.log(`Utils - _replaceAllRank - itemSource`), itemSource)
+        const pathId = itemSource.system.path
+        let profiles = actor.profiles().filter((p) => p.paths.filter((o) => o.id === pathId))
+        if (CONFIG.debug.co?.rolls) console.debug(Utils.log(`Utils - _replaceAllRank - profiles`), profiles)
+        if (profiles && profiles.length === 1) {
+          let compteur = 0
+          //Toutes les voies du profil qui ont atteind le rang demandé
+          let profile = profiles[0]
+          profile.paths.forEach((p) => {
+            const path = actor.items.get(p)
+            if (path.system.rank >= level) compteur += 1
+          })
+          content = content.replace("@allrank[" + extract + "]", compteur)
+        }
+      }
+    }
+    return content
+  }
+
+  /**
+   * Fonction qui va prendre un formule en paramètre et va voir s'il faut remplacer des valeurs custom de rang dans la voie ou des dés évolutifs
+   * @param {*} actor : l'acteur concerné
+   * @param {*} formule : une formule de type texte qui peux potentiellement contenir des @rank/@rang ou du dé évolutif
+   * @param {UUID} source The item source's UUID : used for the #rank
+   * @returns renvoi le context modifié
+   */
+  static evaluateFormula(actor, formule, source) {
+    //Si j'ai un élément custom je le remplace par sa valeur
+    if (formule.includes("@rank") || formule.includes("@rang")) {
+      if (source) formule = this._replaceRank(actor, formule, source)
+    }
+    if (formule.includes("@allrank") || formule.includes("@allrang")) {
+      if (source) formule = this._replaceAllRank(actor, formule, source)
+    }
+    //Si j'ai un dé bonus
+    if (formule.includes("d4°")) {
+      formule = Utils._replaceEvolvingDice(actor, formule)
+    }
+    return formule
   }
 }
