cdc6c2cab77218d1a342d6bf137470b2d204b988
Christophe Laudon <ch.laudon@gmail.com>
2025-08-09 15:48:07 +0200

    Fix un bug sur le calcul de la défense maximale

--- module/models/capacity.mjs
+++ module/models/capacity.mjs
@@ -97,6 +97,12 @@ export default class CapacityData extends ItemData {
     return this.charges.current > 0
   }
 
+  /**
+   * Asynchronously retrieves the name of the linked capacity if allowed and available.
+   *
+   * @async
+   * @returns {Promise<string>} The name of the linked capacity, or an empty string if not available.
+   */
   async getLinkedCapacityName() {
     if (this.allowLinkedCapacity && this.linkedCapacity) {
       const linkedCapacity = await fromUuid(this.linkedCapacity)
@@ -105,6 +111,16 @@ export default class CapacityData extends ItemData {
     return ""
   }
 
+  /**
+   * Retrieves the image associated with the linked capacity, if available.
+   *
+   * If `allowLinkedCapacity` is true and `linkedCapacity` is set, attempts to resolve the linked capacity
+   * using its UUID and returns its image path. If the linked capacity cannot be resolved or is not set,
+   * returns a default image path.
+   *
+   * @async
+   * @returns {Promise<string>} The image path of the linked capacity, or a default image if not available.
+   */
   async getLinkedCapacityImg() {
     if (this.allowLinkedCapacity && this.linkedCapacity) {
       const linkedCapacity = await fromUuid(this.linkedCapacity)
@@ -113,6 +129,12 @@ export default class CapacityData extends ItemData {
     return "systems/co/ui/effects/question.webp"
   }
 
+  /**
+   * Asynchronously retrieves the linked capacity item if linking is allowed and a linked capacity UUID is present.
+   *
+   * @async
+   * @returns {Promise<Object|null>} The linked capacity item object if found, otherwise null.
+   */
   async getLinkedCapacityItem() {
     if (this.allowLinkedCapacity && this.linkedCapacity) {
       const linkedCapacity = await fromUuid(this.linkedCapacity)
@@ -164,7 +186,7 @@ export default class CapacityData extends ItemData {
     if (!this.path) return 0
     // Recherche la voie pour obtenir la défense maximale pour utiliser la capacité
     const { id } = foundry.utils.parseUuid(this.path)
-    const path = this.items.get(id)
+    const path = actor.items.get(id)
     if (!path) return 0
     const maxDefenseArmor = path.system.maxDefenseArmor
     return Math.max(armor.system.defense - maxDefenseArmor, 0)
