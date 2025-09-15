8357b902f29fa70453a3bc9b4cc0e0ae2008e06c
Christophe Laudon <ch.laudon@gmail.com>
2025-09-15 13:08:57 +0200

    Déplacement du fichier co.css dans le répertoire css
    Pour faciliter le hot reloading

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -5,16 +5,19 @@ import less from "gulp-less"
 /*  Compile LESS
 /* ----------------------------------------- */
 const LESS_SRC = "./styles/co.less"
-const CSS_DEST = "."
+const CSS_DEST = "./css"
 const LESS_WATCH = ["./styles/**/*.less"]
 
 function compileLESS() {
-  console.log("Compiling LESS files from:", LESS_SRC, "to:", CSS_DEST);
-  return gulp.src(LESS_SRC)
-    .pipe(less().on('error', function(err) {
-      console.error('LESS Error:', err.message);
-      this.emit('end');
-    }))
+  console.log("Compiling LESS files from:", LESS_SRC, "to:", CSS_DEST)
+  return gulp
+    .src(LESS_SRC)
+    .pipe(
+      less().on("error", function (err) {
+        console.error("LESS Error:", err.message)
+        this.emit("end")
+      }),
+    )
     .pipe(gulp.dest(CSS_DEST))
 }
 const css = gulp.series(compileLESS)
